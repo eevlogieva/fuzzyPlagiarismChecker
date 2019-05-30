4c1db15566d42828069c028b7240d72b97b7e6db
Evgeniya Evlogieva <evgeniya.evlogieva@scigility.com>
2019-05-30 11:20:51 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fuzzyplagiarismchecker',
+    version='1.0.1',
+    description='Check files for plagiarism using fuzzy hashing',
+    author='Evgeniya Evlogieva',
+    author_email='eevlogieva94@gmail.com',
+    url='https://github.com/eevlogieva/fuzzyPlagiarismChecker',
+    packages=find_packages()
+)
