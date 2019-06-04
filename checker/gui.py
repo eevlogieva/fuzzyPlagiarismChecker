5b6a2208127334b0de8a45c929d631e2b6773174
Evgeniya Evlogieva <evgeniya.evlogieva@scigility.com>
2019-06-04 12:50:30 +0200

    Start work on the gui

--- checker/gui.py
+++ checker/gui.py
@@ -2,32 +2,38 @@ import PySimpleGUI as sg
 from ssdeepComparator.ssdeep_comparator import Comparator
 
 
-def extract_filenames():
-    event, (filename, filename2) = sg.Window('Compare two files for similarities'). Layout([[sg.Text('First file')], [sg.Input(), sg.FileBrowse()], 
-       [sg.Text('Second file')], [sg.Input(), sg.FileBrowse()], [sg.OK(), sg.Cancel()]]).Read()
-    return (filename, filename2)
+class GUI:
+    def extractUsecase(self):
+        layout = [[sg.InputCombo(['cmpTwoFiles', 'checkStructure', 'checkStructureDir', 'cmpFile2Dir', 'cmpFilesInDir'])], [sg.OK(), sg.Cancel()]]
+        (button, usecase_arr) = sg.Window('Choose a usecase').Layout(layout).Read()
+        return usecase_arr[0]
 
+    def extract_filenames(self):
+        event, (filename, filename2) = sg.Window('Compare two files for similarities'). Layout([[sg.Text('First file')], [sg.Input(), sg.FileBrowse()], 
+            [sg.Text('Second file')], [sg.Input(), sg.FileBrowse()], [sg.OK(), sg.Cancel()]]).Read()
+        return (filename, filename2)
 
-def extract_file_and_dir():
-    event, (filename, dirname) = sg.Window('Get filename to check'). Layout([[sg.Text('Filename')], [sg.Input(), sg.FileBrowse()],
-        [sg.Text('Dirname')], [sg.Input(), sg.FolderBrowse()], [sg.OK(), sg.Cancel()]]).Read()
+    def extract_file_and_dir(self):
+        event, (filename, dirname) = sg.Window('Get filename to check'). Layout([[sg.Text('Filename')], [sg.Input(), sg.FileBrowse()],
+            [sg.Text('Dirname')], [sg.Input(), sg.FolderBrowse()], [sg.OK(), sg.Cancel()]]).Read()
 
-    return (filename, dirname)
+        return (filename, dirname)
 
+    def check_file_2_file(self):
+        fileNameToCheck, fileName2ToCheck = self.extract_filenames()
 
-def check_file_2_file_gui():
-    fileNameToCheck, fileName2ToCheck = extract_filenames()
+        comparator = Comparator()
+        sg.Popup("The system successfully compared the two files.\n" + "Percentage similarity: " + str(comparator.compare(fileNameToCheck, fileName2ToCheck)))
 
-    comparator = Comparator()
-    sg.Popup("The system successfully compared the two files.\n" + "Percentage similarity: " + str(comparator.compare(fileNameToCheck, fileName2ToCheck)))
+    def check_file_2_dir(self):
+        fileNameToCheck, dirName2ToCheck = self.extract_file_and_dir()
 
-
-def check_file_2_dir_gui():
-    fileNameToCheck, dirName2ToCheck = extract_file_and_dir()
-
-    comparator = Comparator()
-    sg.Popup('Result', "Similar files: " + str(comparator.compare(fileNameToCheck, dirName2ToCheck)))
+        comparator = Comparator()
+        sg.Popup('Result', "Similar files: " + str(comparator.compare(fileNameToCheck, dirName2ToCheck)))
 
 
 if __name__ == '__main__':
-    check_file_2_file_gui()
+    gui = GUI()
+    print(gui.extractUsecase())
+    # print(usecase)
+    # gui.check_file_2_file()
