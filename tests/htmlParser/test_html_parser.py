9b3e97b756a869faea9325151838ea0eb03e0cab
Evgeniya Evlogieva <evgeniya.evlogieva@scigility.com>
2019-07-10 11:53:23 +0200

    Fix the htmlParser test

--- tests/htmlParser/test_html_parser.py
+++ tests/htmlParser/test_html_parser.py
@@ -7,4 +7,5 @@ def test_parseTags():
     fileContent = open('./resources/simpleFile.html', 'r').read()
     htmlParser = MyHTMLParser()
     htmlParser.feed(fileContent)
-    assert htmlParser.getTags() == "<html><head></head><body></body></html>"
+    assert htmlParser.getTags() == '<html><head></head><body></body></html>'
+    assert htmlParser.getContent().strip() == 'This is the head.\n\n\n\n\nThis is the body.'
